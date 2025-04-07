650bb5312c027c21c37041ec9fec76c4d2b4520b
Diva Zahra Fakhirah <dvazahr31@gmail.com>
2025-04-08 00:53:02 +0700

    Membuat tampilan about

--- app/src/main/java/com/divazahra0070/asesmen1mobpro/navigation/NavGraph.kt
+++ app/src/main/java/com/divazahra0070/asesmen1mobpro/navigation/NavGraph.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.divazahra0070.asesmen1mobpro.ui.screen.AboutScreen
 import com.divazahra0070.asesmen1mobpro.ui.screen.MainScreen
 
 @Composable
@@ -15,6 +16,9 @@ fun SetupNavGraph(navController: NavHostController = rememberNavController()) {
     ) {
         composable(route = Screen.Home.route){
             MainScreen()
+        }
+        composable(route = Screen.About.route){
+            AboutScreen()
         }
     }
 }
--- app/src/main/java/com/divazahra0070/asesmen1mobpro/navigation/Screen.kt
+++ app/src/main/java/com/divazahra0070/asesmen1mobpro/navigation/Screen.kt
@@ -2,4 +2,5 @@ package com.divazahra0070.asesmen1mobpro.navigation
 
 sealed class Screen(val route: String) {
     data object Home: Screen("mainScreen")
+    data object About: Screen("aboutScreen")
 }
